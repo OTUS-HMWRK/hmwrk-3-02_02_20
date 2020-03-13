3952f58cef1f629069dce78ce10f90f0860eab6c
kirill iudov <kirill.iudov@yandex.ru>
2020-03-13 17:45:57 +0300

    ip class

--- app/app.cpp
+++ app/app.cpp
@@ -6,6 +6,49 @@
 #include <algorithm>
 #include <charconv>
 
+namespace IP {
+
+class IPv4 {
+public:
+    uint32_t IP;
+    uint8_t 
+        *p1 = (uint8_t *)&IP, 
+        *p2 = p1 + 1, 
+        *p3 = p1 + 2, 
+        *p4 = p1 + 3;
+    
+    uint8_t * aIP = p1;
+    IPv4(const uint32_t ip = 0) : IP(ip) {}
+    IPv4(const uint8_t p1, const uint8_t p2, const uint8_t p3, const uint8_t p4) {
+        aIP[0] = p1;
+        aIP[1] = p2;
+        aIP[2] = p3;
+        aIP[3] = p4;    
+    }
+    IPv4(const IPv4 &ip) : IP(ip.IP) {}
+    IPv4(const std::string &str) {
+        try {
+            auto point = str.c_str();
+            for (auto i = 0; i < 4; i++) {
+                if(auto [p, ec] = std::from_chars(point, str.c_str() + str.size(), aIP[i]); ec == std::errc()){
+                    point = ++p;
+                    std::cout << "p: " << (int)p << ", value: " << (int)aIP[i] << std::endl;
+                } else {
+                    throw 20;
+    
+                }
+                
+            }
+        } 
+        catch(const std::exception &e) {
+            std::cout << " a standard exception was caught, with message '"
+                  << e.what() << "'\n";
+        }
+    }
+};
+
+}
+
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
 // ("..", '.') -> ["", "", ""]
@@ -99,6 +142,15 @@ int main(int, char const **)
 {
     try
     {
+        //IP::IPv4 testIP(512);
+        //IP::IPv4 testIP{0,0,1,0};
+        IP::IPv4 testIP("123.345.0.99");
+        std::cout << "testIP.IP: " << testIP.IP << std::endl;
+        std::cout << "testIP.aIP[0]: " << (int)testIP.aIP[0] << std::endl;
+        std::cout << "testIP.aIP[1]: " << (int)testIP.aIP[1] << std::endl;
+        std::cout << "testIP.aIP[2]: " << (int)testIP.aIP[2] << std::endl;
+        std::cout << "testIP.aIP[3]: " << (int)testIP.aIP[3] << std::endl;
+
         std::vector<std::vector<std::string> > ip_pool;
 
         for(std::string line; std::getline(std::cin, line);)
@@ -116,7 +168,7 @@ int main(int, char const **)
             comparator
         );
 
-        print_ip_pool(ip_pool);
+        //print_ip_pool(ip_pool);
 
         // 222.173.235.246
         // 222.130.177.64
@@ -129,18 +181,19 @@ int main(int, char const **)
         // TODO filter by first byte and output
         // ip = filter(1)
 
-        std::vector<std::vector<std::string>> ip_pool_1;
-        std::transform(
+        std::vector<std::vector<std::string>>  ip_pool_1;
+        std::for_each(
             ip_pool.begin(), 
-            ip_pool.end(), 
-            ip_pool_1.begin(), 
-            [](const std::vector<std::string> i)->std::string
-            {
-                if ("1" == i[0]) return i[0];
+            ip_pool.end(),  
+            [&ip_pool_1](std::vector<std::string> i)
+            {   
+                if ("1" == i[0])
+                    ip_pool_1.push_back(i);             
             }
         );
-
-        print_ip_pool(ip_pool);
+        //std::cout << "ip_pool_1 size: " << ip_pool_1.size() << std::endl;
+        
+        //print_ip_pool(ip_pool_1);
 
 
         // 1.231.69.33
@@ -152,6 +205,20 @@ int main(int, char const **)
         // TODO filter by first and second bytes and output
         // ip = filter(46, 70)
 
+        std::vector<std::vector<std::string>>  ip_pool_2;
+        std::for_each(
+            ip_pool.begin(), 
+            ip_pool.end(),  
+            [&ip_pool_2](std::vector<std::string> i)
+            {   
+                if ("46" == i[0] && "70" == i[1])
+                    ip_pool_2.push_back(i);             
+            }
+        );
+        //std::cout << "ip_pool_2 size: " << ip_pool_2.size() << std::endl;
+        
+        //print_ip_pool(ip_pool_2);
+
         // 46.70.225.39
         // 46.70.147.26
         // 46.70.113.73
@@ -159,6 +226,20 @@ int main(int, char const **)
 
         // TODO filter by any byte and output
         // ip = filter_any(46)
+
+        std::vector<std::vector<std::string>>  ip_pool_3;
+        std::for_each(
+            ip_pool.begin(), 
+            ip_pool.end(),  
+            [&ip_pool_3](std::vector<std::string> i)
+            {   
+                if (("46" == i[0]) || ("46" == i[1]) || ("46" == i[2]) || ("46" == i[3]))
+                    ip_pool_3.push_back(i);             
+            }
+        );
+        //std::cout << "ip_pool_1 size: " << ip_pool_3.size() << std::endl;
+        
+        //print_ip_pool(ip_pool_3);
 
         // 186.204.34.46
         // 186.46.222.194
